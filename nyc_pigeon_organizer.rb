cff5750bbb35b296bb1f7a0c7a15a3b178df3991
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-27 00:27:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
-require 'pry' 
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
 
-def ny
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  new_hash
+end
